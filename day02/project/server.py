8f91bc3a3307eb351c15f232d181c625be46736a
sige123 <837337164@qq.com>
2019-06-27 23:16:20 +0800

    tornadoday02

---
+++ day02/project/server.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+import tornado.web
+import tornado.ioloop
+import tornado.httpserver
+import config
+
+class IndexHandler(tornado.web.RequestHandler):
+    def get(self, *args, **kwargs):
+        self.write("sunck is a good man")
+
+if __name__ == "__main__":
+    app = tornado.web.Application([
+        (r"/",IndexHandler)
+    ])
+    httpServer = tornado.httpserver.HTTPServer(app)
+    httpServer.bind(config.options.port)
+    httpServer.start(1)
+    tornado.ioloop.IOLoop.current().start()
+
+
+
