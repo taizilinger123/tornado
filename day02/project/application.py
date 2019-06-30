249a2e9419d43edb686dbafb971cc5921238414b
sige <837337164@qq.com>
2019-06-30 22:57:01 +0800

    2019-6-30

--- day02/project/application.py
+++ day02/project/application.py
@@ -7,6 +7,9 @@ from views import index
 class Application(tornado.web.Application):
     def __init__(self):
         handlers = [
-            (r'/',index.IndexHandler)
+            (r'/',index.IndexHandler),
+            (r'/sunck', index.SunckHandler, {"word1":"good", "word2":"nice"}),
+            ()
+
         ]
         super(Application,self).__init__(handlers,**config.settings)
--- day02/project/views/index.py
+++ day02/project/views/index.py
@@ -7,3 +7,13 @@ class IndexHandler(RequestHandler):
     def get(self, *args, **kwargs):
         self.write("sunck is a good man")
 
+class SunckHandler(RequestHandler):
+    #该方法会在HTTP方法之前调用
+    def initialize(self, word1, word2):
+        self.word1 = word1
+        self.word2 = word2
+
+    def get(self, *args, **kwargs):
+        print(self.word1, self.word2)
+        self.write("sunck is a nice man")
+
